2493ae6610d8fc4ba90a23a6f46874c26284fc0c
TegwynTwmffat <paddy@goatindustries.co.uk>
2022-06-30 10:32:31 +0100

    Add files via upload

---
+++ Testing/Heltech_ESP32_temperature_transmitter_01/soil.h
@@ -0,0 +1,10 @@
+#ifndef WEATHER_STATION_V3_SOIL_H
+#define WEATHER_STATION_V3_SOIL_H
+
+extern void soil();
+extern void soilSetup();
+
+extern int moisture;
+
+
+#endif
--- Testing/test_pyserial_2.py
+++ Testing/test_pyserial_2.py
@@ -28,7 +28,7 @@ sys.stderr.write("\x1b[94m" + "Start: " + '\x1b[0m' + end)
 #initialize serial port
 ser = serial.Serial()
 ser.port = '/dev/ttyUSB0' #Arduino serial port
-ser.baudrate = 57600
+ser.baudrate = 115200
 ser.timeout = 10 #specify timeout when using readline()
 ser.open()
 if ser.is_open==True:
@@ -38,12 +38,13 @@ if ser.is_open==True:
 # Create figure for plotting
 fig = plt.figure(figsize=(10,5))
 ax = fig.add_subplot(1, 1, 1)
-xs = [] # store trials here (n)
-ys = [] # store relative frequency here
+xs = [] # time index
+ys = [] # temperature values
+ym = [] # moisture values
 rs = [] # for theoretical probability
 
 # This function is called periodically from FuncAnimation
-def animate(i, xs, ys):
+def animate(i, xs, ys, ym):
 
     #Aquire and parse data from serial port
     character_data=ser.readline()      # ascii
@@ -51,30 +52,44 @@ def animate(i, xs, ys):
     character_data = character_data[0:-2]
     len_character_data = len(character_data)
     
-    if len_character_data >2 and len_character_data <20:
+    # Find the occurrance of character 'i':
+    i = 0
+    for c in character_data:
+        if c == 'i':
+            print("character m was detected at index value: ",i)
+            break
+        i = i + 1
+    
+    if len_character_data >2 and len_character_data <50:
     	
         print("len_character_data:",len_character_data)
         print("character_data:",character_data)
-        character_data_truncated = character_data[11:len_character_data]
-        print("character_data_truncated:",character_data_truncated)
+        character_data_truncated_temp = character_data[11:(i-3)]
+        print("character_data_truncated_temp:",character_data_truncated_temp)
+        data_y_temp = float(character_data_truncated_temp)
+        character_data_truncated_mois = character_data[(i+7):len_character_data]
+        print("character_data_truncated_mois:",character_data_truncated_mois)
         print("")
-        data_y = float(character_data_truncated)
+        data_y_mois = float(character_data_truncated_mois) - 4095 + 20
 	
-	    # Add x and y to lists
+        # Add x and y to lists
         # xs.append(i)
-        ys.append(data_y)
+        ys.append(data_y_temp)
+        ym.append(data_y_mois)
         xs = range(len(ys))
         print("xs: ",xs)
         print("ys: ",ys)
-        # rs.append(0.5)
+        print("ym: ",ym)
 
         # Limit x and y lists to 2000 items
         xs = xs[-2000:]
         ys = ys[-2000:]
+        ym = ym[-2000:]
 
         # Draw x and y lists
         ax.clear()
         ax.plot(xs, ys, label="Temperature")
+        ax.plot(xs, ym, label="Moisture")
         # ax.plot(xs, rs, label="Theoretical Probability")
 
         # Format plot
@@ -88,7 +103,7 @@ def animate(i, xs, ys):
         # plt.axis([1, 100, 0, 1.1]) # Use for 100 trial demo
 
 # Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys, ym), interval=1000)
 plt.show()
 
 
